1acf04187c45310acd13e147e807f3772fee9c67
Dipesh <dipesh1dip1@gmail.com>
2026-03-06 22:58:26 +0545

    update

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-%t^&f2u_wtk=tr7_!gt_ksnc7_+89wqat(s0z02pmfon#adow3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
